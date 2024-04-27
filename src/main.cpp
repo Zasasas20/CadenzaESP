21873359bbfc15ad202a899418a443c1613b0306
Zasasas20 <zaidassim2013@gmail.com>
2024-04-27 18:42:30 +0200

    Added default radio functionality + General bug fixing!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
 #include <Audio.h>
 #include <Preferences.h>
+#include <SPIFFS.h>
 #include "nvs_flash.h"
 
 #define MAXCONNECT 5
@@ -15,22 +16,25 @@
 
 #define VOL 39
 
+struct {
+  const char * Link;
+  const char * Type;
+} AudioInfo;
+
 int volume = 10;
 
 Audio audio;
 
-WebServer server(80);
+StaticJsonDocument<200> doc_rx;
+StaticJsonDocument<200> doc_tx;
+
+AsyncWebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 Preferences pref;
-
-StaticJsonDocument<200> doc_tx;
-StaticJsonDocument<200> doc_rx;
 
 const char * AP_SSID = "CadenzaAP";
 const char * AP_password = "PASSWORD";
 
-String webText = "<!DOCTYPE html><html><style>html, body{ margin: 0; height: 100%;}body{ display: flex; justify-content: center; align-items: center;}#Elements{ display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 20px;}</style><head> <title>Cadenza Setup</title></head><body> <div id = 'Elements'> <div id = 'Info'> <div class = 'textBox'> <p>Network Name: </p> <input type='text' id = 'SSID'> </div> <div class = 'textBox'> <p>Password: </p> <input type='text' id = 'Password'> </div> </div> <div id = 'Button'> <button id='Submit'>Connect</button> </div> </div></body><script> var Socket; document.getElementById('Submit').addEventListener('click', send_info); function init(){ Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) {}; } function send_info(){ var data = { SSID: document.getElementById('SSID').value, PASSWORD: document.getElementById('Password').value }; console.log(data); Socket.send(JSON.stringify(data)); } window.onload = function(event){ init(); console.log('Webiste Loads'); }</script></html>";
-String musicText = "<!DOCTYPE html><html><style>html, body{ margin: 0; height: 100%;}body{ display: flex; justify-content: center; align-items: center;}#Info{ display: flex; align-items:end; gap: 20px;}#Elements{ display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 20px;}</style><head> <title>Cadenza</title></head><body> <div id = 'Elements'> <div id = 'Menu'> <label for='Radio-Channel'>Choose a radio category:</label> <select name='Channels' id='Channels'> <option value='https://strm112.1.fm/bossanova_mobile_mp3?aw_0_req.gdpr=true'>Bossa Nova</option> <option value='http://stream.antennethueringen.de/live/aac-64/stream.antennethueringen.de/'>Hip Hop</option> <option value='http://curiosity.shoutca.st:6035/;'>Arabic</option> <option value='Custom'>Custom</option> </select> </div> <div id = 'Info'> <div class = 'textBox' id='Box' hidden> <p>Radio Channel URL: </p> <input type='text' id = 'Music'> </div> <div class = 'Button'> <button id='Change'>Change Channel</button> </div> </div> <div class = 'Button'> <button id='Reset'>Reset Settings</button> </div> </div></body><script> var Socket; document.getElementById('Reset').addEventListener('click', reset); document.getElementById('Change').addEventListener('click', change); document.getElementById('Channels').addEventListener('change', category); function init(){ Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) {}; } function reset(){ var data = { Command: 'Reset' }; Socket.send(JSON.stringify(data)); } function change(){ var data = { Command: 'Change' }; if (document.getElementById('Channels').value === 'Custom'){ data.Link = document.getElementById('Music').value; } else{ data.Link = document.getElementById('Channels').value; } Socket.send(JSON.stringify(data)); console.log(data); } function category(){ let choice = document.getElementById('Channels').value; if (choice === 'Custom'){ document.getElementById('Box').hidden = false; } else{ document.getElementById('Box').hidden = true; } } window.onload = function(event){ init(); console.log('Website Loads'); }</script></html>";
 IPAddress AP_IP(192,167,1,22);
 IPAddress AP_GATEWAY(192,167,1,5);
 IPAddress AP_SUBNET(255,255,255,0);
@@ -43,6 +47,11 @@ bool isSetup();
 void writeNetwork(const char * ID, const char * PASS);
 void reconnectToWifi();
 void resetConfigs();
+void writeDefault(const char * Link);
+void sendNew(const char * M_Type, const char * M_Link);
+void loadAudio();
+void resetAudio();
+void update();
 
 void resetConfigs(){
   Serial.println("Resetting");
@@ -51,6 +60,15 @@ void resetConfigs(){
   Serial.println("Reset complete, restarting...");
   delay(1000);
   ESP.restart();
+}
+
+void resetAudio(){
+  Serial.println("Resetting audio");
+  writeDefault("");
+}
+
+void update(){
+  audio.connecttohost(AudioInfo.Link);
 }
 
 void connectionHandler(byte num, WStype_t type, uint8_t * payload, size_t length){
@@ -66,17 +84,32 @@ void connectionHandler(byte num, WStype_t type, uint8_t * payload, size_t length
         Serial.print("Received command: ");
         Serial.println(Command);
         if (strcmp(Command, "Reset") == 0){
-          resetConfigs();
+          const char * Config = doc_rx["Config"];
+          if (strcmp(Config, "All") == 0){
+            resetConfigs();
+          }
+          else{
+            resetAudio();
+          }
+        }
+        else if (strcmp(Command, "Default") == 0){
+          writeDefault(doc_rx["Link"]);
         }
         else{
-          const char* Channel = doc_rx["Link"];
-          audio.connecttohost(Channel);
+          AudioInfo.Link = doc_rx["Link"];
+          AudioInfo.Type = doc_rx["Type"];
+          update();
+          sendNew(AudioInfo.Type, AudioInfo.Link);
         }
       }
+  }
+  else if (type == WStype_CONNECTED){
+    sendNew(AudioInfo.Type, AudioInfo.Link);
   }
 }
 
 void setupHandler(byte num, WStype_t type, uint8_t * payload, size_t length){
+    StaticJsonDocument<200> doc_rx;
     if (type == WStype_TEXT){
       DeserializationError error = deserializeJson(doc_rx, payload);
       if (error) {
@@ -92,7 +125,7 @@ void setupHandler(byte num, WStype_t type, uint8_t * payload, size_t length){
           Serial.println("Stored");
           WiFi.softAPdisconnect(true);
           Serial.println("Disconnected");
-          server.stop();
+          server.end();
           webSocket.close();
           Serial.println("Successfully closed");
           delay(200);
@@ -131,13 +164,16 @@ void connectToWifi(){
 
   Serial.print("Connected on: ");
   Serial.println(WiFi.localIP());
-  server.on("/", [](){
-    server.send(200, "text\html", musicText);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request){
+    request->send(SPIFFS, "/musicplayer.html", "text/html");
   });
-  server.begin();
+
+  server.serveStatic("/", SPIFFS, "/");
 
   webSocket.begin();
   webSocket.onEvent(connectionHandler);
+
+  server.begin();
 }
 
 void APMode(){
@@ -152,15 +188,18 @@ void APMode(){
 
   Serial.println(WiFi.softAPIP());
 
-  server.on("/", [](){
-    server.send(200, "text\html", webText);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request){
+    request->send(SPIFFS, "/setup.html", "text/html");
   });
-  server.begin();
-  Serial.println("Server Initialized");
+
+  server.serveStatic("/", SPIFFS, "/");
 
   webSocket.begin();
   webSocket.onEvent(setupHandler);
   Serial.println("Websocket Initialized");
+
+  server.begin();
+  Serial.println("Server Initialized");
 }
 
 bool isSetup(){
@@ -179,9 +218,37 @@ void writeNetwork(const char * ID, const char * PASS){
   pref.end();
 }
 
+void writeDefault(const char * Link){
+  Serial.println("Writing details");
+  pref.begin("musicDetails", false);
+  pref.putString("Default", Link);
+  pref.end();
+}
+
+void sendNew(const char * M_Type, const char * M_Link){
+  String Jstring = "";
+  StaticJsonDocument<200> doc_tx;
+  JsonObject object = doc_tx.to<JsonObject>();
+  object["Command"] = "New";
+  object["Type"] = M_Type;
+  object["Link"] = M_Link;
+  serializeJson(doc_tx, Jstring);
+  webSocket.broadcastTXT(Jstring);
+}
+
+void loadAudio(){
+  Serial.println("Reading details");
+  pref.begin("musicDetails", true);
+  audio.connecttohost(pref.getString("Default", "").c_str());
+  pref.end();
+}
 
 void setup() {
   Serial.begin(9600);
+
+  if(!SPIFFS.begin()){
+    Serial.println("Failed to initialize SPIFFS");
+  }
 
   if (isSetup()){
     connectToWifi();
@@ -193,10 +260,11 @@ void setup() {
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
 
   audio.setVolume(volume);
+
+  loadAudio();
 }
 
 void loop() {
-  server.handleClient();
   webSocket.loop();
   audio.loop();
   volume = map ((analogRead(VOL)), 0, 4095, 0, 20);
